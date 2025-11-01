3892885a19acfc2f47911283056148582099455d
Student-Aaditya <aadityakuamr518@gmail.com>
2025-11-02 01:04:22 +0530

    adding data

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,50 @@
+require("dotenv").config();
 const express=require("express");
+const app=express();
+const port=process.env.PORT;
+const {spawn} =require("child_process");
+const cors=require("cors");
+const responses=require("./config/data.js");
+
+app.use(cors({
+  origin: ["http://localhost:5173", "https://chatbot-ef9j.onrender.com"],
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type"],
+}));
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("hello");
+})
+app.post("/chat", (req, res) => {
+  const userMessage = req.body.message;
+  const python = spawn("python", ["../model_python/predict_intent.py", userMessage]);
+
+
+  python.stdout.on("data", (data) => {
+    const intent = data.toString().trim();
+
+    const response = responses[intent] || responses.default;
+    if (!res.headersSent) {
+      res.json({ intent, response });
+    }
+  });
+
+  python.stderr.on("data", (data) => {
+    console.error(`Error: ${data}`);
+    if (!res.headersSent) {
+      res.status(500).json({ error: "Model error",msg:data.toString() });
+    }
+  });
+
+  python.on("close", (code) => {
+    if (!res.headersSent && code !== 0) {
+      res.status(500).json({ error: "Python process exited unexpectedly" });
+    }
+  });
+});
+
+app.listen(port,()=>{
+    console.log(`server working on ${port}`);
+})
