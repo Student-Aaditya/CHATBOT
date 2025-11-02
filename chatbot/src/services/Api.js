f76301306e4bb7842c9c6951a5120aa43ca2ab3b
Student-Aaditya <aadityakuamr518@gmail.com>
2025-11-03 00:33:43 +0530

    Deleting chatbot folder

--- chatbot/src/services/Api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:5498/chat";
-
-export const sendMessage = async (message) => {
-  const res = await axios.post(API_URL, { message });
-  return res.data;
-};
